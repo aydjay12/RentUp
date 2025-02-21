ac44b14af5869f237f67313dec4e0c4cf4619e70
Ayodeji <frontend@sphiderassweb.org>
2025-02-21 18:27:42 +0100

    added backend

--- server/index.js
+++ server/index.js
@@ -24,7 +24,7 @@ const PORT = process.env.PORT || 3000;
 
 // Middleware
 app.use(cookieParser());
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({ origin: "https://rent-up-gold.vercel.app", credentials: true }));
 app.use(express.json({ limit: "10mb" }));
 
 const __dirname = path.resolve();
