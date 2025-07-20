36fda690645580c9a75f44b3e92fed5af2ab670f
aydjay12 <aydjay12@example.com>
2025-07-20 17:47:35 +0100

    changed client url

--- server/utils/generateTokenAndSetCookie.js
+++ server/utils/generateTokenAndSetCookie.js
@@ -7,8 +7,8 @@ export const generateTokenAndSetCookie = (res, userId) => {
 
 	res.cookie("token", token, {
 		httpOnly: true,
-		secure: process.env.NODE_ENV === "production",
-		sameSite: "strict",
+		secure: true, // must be true for cross-site cookies
+		sameSite: "none", // must be 'none' for cross-site cookies
 		maxAge: 7 * 24 * 60 * 60 * 1000,
 	});
 
