c7aab4901d5d83eb75e357ead5b0606d08a8a4f0
aydjay12 <aydjay12@gmail.com>
2026-01-30 22:37:55 +0100

    using cron

--- server/index.js
+++ server/index.js
@@ -22,10 +22,12 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(cookieParser());
-app.use(cors({ origin: [
-  "https://rent-up-gold.vercel.app",
-  "http://localhost:5173"
-], credentials: true }));
+app.use(cors({
+  origin: [
+    "https://rent-up-gold.vercel.app",
+    "http://localhost:5173"
+  ], credentials: true
+}));
 
 app.use(express.json({ limit: "10mb" }));
 
@@ -38,6 +40,10 @@ cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+app.get("/ping", (req, res) => {
+  res.status(200).send("pong");
 });
 
 app.use("/api/auth", authRoute(upload));
